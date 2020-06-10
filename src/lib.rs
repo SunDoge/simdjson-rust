71fa268434b9c7d88c734968b5dc273e0b36bd75
SunDoge <384813529@qq.com>
2020-06-10 14:21:11 +0800

    Testing CXX

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-pub mod error;
-pub mod json_ioutil;
-pub mod json_parser;
-pub mod parsed_json;
-pub mod parsed_json_iterator;
+// pub mod error;
+// pub mod json_ioutil;
+// pub mod json_parser;
+// pub mod parsed_json;
+// pub mod parsed_json_iterator;
 
-pub use error::SimdJsonError;
-pub use json_parser::{build_parsed_json, json_parse};
-pub use parsed_json::{ParsedJson, DEFUALT_MAX_DEPTH};
-pub use parsed_json_iterator::ParsedJsonIterator;
+// pub use error::SimdJsonError;
+// pub use json_parser::{build_parsed_json, json_parse};
+// pub use parsed_json::{ParsedJson, DEFUALT_MAX_DEPTH};
+// pub use parsed_json_iterator::ParsedJsonIterator;
 
 #[cfg(test)]
 mod tests {

0359d725b761a36cab42dff2dc368f292679e361
SunDoge <384813529@qq.com>
2020-06-13 00:25:27 +0800

    Test ElementResult

--- csrc/wrapper.cpp
+++ csrc/wrapper.cpp
@@ -1,5 +1,6 @@
 #include "wrapper.h"
 #include <iostream>
+#include "src/libsimdjson.rs.h"
 
 namespace simdjson
 {
@@ -13,125 +14,137 @@ namespace simdjson
             return std::make_unique<parser>(max_capacity);
         }
 
-        std::unique_ptr<element> parser_load(parser &p, rust::Str path)
+        std::unique_ptr<element> parser_load(parser &p, rust::Str path, int &code)
         {
+            element value;
+            error_code error;
             const std::string &cpath = std::string(path);
-            auto result = p.load(cpath);
-            return std::make_unique<element>(result.value());
+            p.load(cpath).tie(value, error);
+            code = int(error);
+            return std::make_unique<element>(value);
         }
 
-        std::unique_ptr<element> parser_parse_string(parser &p, rust::Str s)
-        {
-            const std::string &cs = std::string(s);
-            auto result = p.parse(cs);
-            return std::make_unique<element>(result.value());
-        }
-
-        std::unique_ptr<element> parser_parse_padded_string(parser &p, const padded_string &s)
-        {
-            auto result = p.parse(s);
-            return std::make_unique<element>(result.value());
-        }
-
-        std::unique_ptr<padded_string> padded_string_from_string(rust::Str s)
-        {
-            const std::string &cs = std::string(s);
-            return std::make_unique<padded_string>(cs);
-        }
-
-        // uint8_t tape_ref_type(const tape_ref &tr)
+        // std::unique_ptr<element> parser_parse_string(parser &p, rust::Str s, int &code)
         // {
-        //     auto tape_type = tr.tape_ref_type();
-        //     return static_cast<uint8_t>(tape_type);
+        //     element value;
+        //     error_code error;
+        //     const std::string &cs = std::string(s);
+        //     p.parse(cs).tie(value, error);
+        //     code = int(error);
+        //     return std::make_unique<element>(value);
         // }
 
-        // uint64_t tape_ref_next_tape_value(const tape_ref &tr)
+        // std::unique_ptr<element> parser_parse_padded_string(parser &p, const padded_string &s, int &code)
         // {
-        //     auto value = tr.next_tape_value<uint64_t>();
-        //     return value;
+        //     element value;
+        //     error_code error;
+        //     p.parse(s).tie(value, error);
+        //     code = int(error);
+        //     return std::make_unique<element>(value);
         // }
 
-        rust::Str element_get_string(const element &elm)
-        {
-            auto result = elm.get<const char *>();
-            return rust::Str(result.value());
-        }
+        // std::unique_ptr<padded_string> padded_string_from_string(rust::Str s)
+        // {
+        //     const std::string &cs = std::string(s);
+        //     return std::make_unique<padded_string>(cs);
+        // }
 
-        std::unique_ptr<array> element_get_array(const element &elm)
-        {
-            auto result = elm.get<array>();
-            return std::make_unique<array>(result.value());
-        }
+        // // uint8_t tape_ref_type(const tape_ref &tr)
+        // // {
+        // //     auto tape_type = tr.tape_ref_type();
+        // //     return static_cast<uint8_t>(tape_type);
+        // // }
 
-        std::unique_ptr<object> element_get_object(const element &elm)
-        {
-            auto result = elm.get<object>();
-            return std::make_unique<object>(result.value());
-        }
+        // // uint64_t tape_ref_next_tape_value(const tape_ref &tr)
+        // // {
+        // //     auto value = tr.next_tape_value<uint64_t>();
+        // //     return value;
+        // // }
 
-        uint64_t element_get_number(const element &elm)
-        {
-            auto result = elm.get<uint64_t>();
-            return result.value();
-        }
+        // rust::Str element_get_string(const element &elm, int &code)
+        // {
+        //     const char *value;
+        //     error_code error;
+        //     elm.get<const char *>().tie(value, error);
+        //     code = int(error);
+        //     return rust::Str(value);
+        // }
 
-        bool element_is_null(const element &elm)
-        {
-            return elm.is_null();
-        }
+        // std::unique_ptr<array> element_get_array(const element &elm)
+        // {
+        //     auto result = elm.get<array>();
+        //     return std::make_unique<array>(result.value());
+        // }
 
-        std::unique_ptr<element> element_at(const element &elm, rust::Str s)
-        {
-            auto json_pointer = std::string_view(s.data(), s.size());
-            auto result = elm.at(json_pointer);
-            return std::make_unique<element>(result.value());
-        }
+        // std::unique_ptr<object> element_get_object(const element &elm)
+        // {
+        //     auto result = elm.get<object>();
+        //     return std::make_unique<object>(result.value());
+        // }
 
-        std::unique_ptr<element> element_at_index(const element &elm, size_t index)
-        {
-            auto result = elm.at(index);
-            return std::make_unique<element>(result.value());
-        }
+        // uint64_t element_get_number(const element &elm)
+        // {
+        //     auto result = elm.get<uint64_t>();
+        //     return result.value();
+        // }
 
-        std::unique_ptr<element> element_at_key(const element &elm, rust::Str s)
-        {
-            auto key = std::string_view(s.data(), s.size());
-            auto result = elm.at_key(key);
-            return std::make_unique<element>(result.value());
-        }
+        // bool element_is_null(const element &elm)
+        // {
+        //     return elm.is_null();
+        // }
 
-        std::unique_ptr<element> array_at(const array &arr, rust::Str s)
-        {
-            auto json_pointer = std::string_view(s.data(), s.size());
-            auto result = arr.at(json_pointer);
-            return std::make_unique<element>(result.value());
-        }
+        // std::unique_ptr<element> element_at(const element &elm, rust::Str s)
+        // {
+        //     auto json_pointer = std::string_view(s.data(), s.size());
+        //     auto result = elm.at(json_pointer);
+        //     return std::make_unique<element>(result.value());
+        // }
 
-        std::unique_ptr<element> array_at_index(const array &arr, size_t index)
-        {
-            auto result = arr.at(index);
-            return std::make_unique<element>(result.value());
-        }
+        // std::unique_ptr<element> element_at_index(const element &elm, size_t index)
+        // {
+        //     auto result = elm.at(index);
+        //     return std::make_unique<element>(result.value());
+        // }
 
-        std::unique_ptr<element> object_at(const object &obj, rust::Str s)
-        {
-            auto json_pointer = std::string_view(s.data(), s.size());
-            auto result = obj.at(json_pointer);
-            return std::make_unique<element>(result.value());
-        }
+        // std::unique_ptr<element> element_at_key(const element &elm, rust::Str s)
+        // {
+        //     auto key = std::string_view(s.data(), s.size());
+        //     auto result = elm.at_key(key);
+        //     return std::make_unique<element>(result.value());
+        // }
 
-        std::unique_ptr<element> object_at_key(const object &obj, rust::Str s)
-        {
-            auto key = std::string_view(s.data(), s.size());
-            auto result = obj.at_key(key);
-            return std::make_unique<element>(result.value());
-        }
+        // std::unique_ptr<element> array_at(const array &arr, rust::Str s)
+        // {
+        //     auto json_pointer = std::string_view(s.data(), s.size());
+        //     auto result = arr.at(json_pointer);
+        //     return std::make_unique<element>(result.value());
+        // }
 
-        std::unique_ptr<element> object_at_key_case_insensitive(const object &obj, rust::Str s)
-        {
-            auto key = std::string_view(s.data(), s.size());
-            auto result = obj.at_key_case_insensitive(key);
-            return std::make_unique<element>(result.value());
-        }
+        // std::unique_ptr<element> array_at_index(const array &arr, size_t index)
+        // {
+        //     auto result = arr.at(index);
+        //     return std::make_unique<element>(result.value());
+        // }
+
+        // std::unique_ptr<element> object_at(const object &obj, rust::Str s)
+        // {
+        //     auto json_pointer = std::string_view(s.data(), s.size());
+        //     auto result = obj.at(json_pointer);
+        //     return std::make_unique<element>(result.value());
+        // }
+
+        // std::unique_ptr<element> object_at_key(const object &obj, rust::Str s)
+        // {
+        //     auto key = std::string_view(s.data(), s.size());
+        //     auto result = obj.at_key(key);
+        //     return std::make_unique<element>(result.value());
+        // }
+
+        // std::unique_ptr<element> object_at_key_case_insensitive(const object &obj, rust::Str s)
+        // {
+        //     auto key = std::string_view(s.data(), s.size());
+        //     auto result = obj.at_key_case_insensitive(key);
+        //     return std::make_unique<element>(result.value());
+        // }
     } // namespace ffi
 } // namespace simdjson
--- src/libsimdjson.rs
+++ src/libsimdjson.rs
@@ -1,104 +1,60 @@
 #[cxx::bridge(namespace = simdjson::ffi)]
 pub mod ffi {
 
-    #[repr(u32)]
-    enum error_code {
-        SUCCESS = 0,
-        ///< No error
-        SUCCESS_AND_HAS_MORE,
-        ///< @private No error and buffer still has more data
-        CAPACITY,
-        ///< This parser can't support a document that big
-        MEMALLOC,
-        ///< Error allocating memory, most likely out of memory
-        TAPE_ERROR,
-        ///< Something went wrong while writing to the tape (stage 2), this is a generic error
-        DEPTH_ERROR,
-        ///< Your document exceeds the user-specified depth limitation
-        STRING_ERROR,
-        ///< Problem while parsing a string
-        T_ATOM_ERROR,
-        ///< Problem while parsing an atom starting with the letter 't'
-        F_ATOM_ERROR,
-        ///< Problem while parsing an atom starting with the letter 'f'
-        N_ATOM_ERROR,
-        ///< Problem while parsing an atom starting with the letter 'n'
-        NUMBER_ERROR,
-        ///< Problem while parsing a number
-        UTF8_ERROR,
-        ///< the input is not valid UTF-8
-        UNINITIALIZED,
-        ///< unknown error, or uninitialized document
-        EMPTY,
-        ///< no structural element found
-        UNESCAPED_CHARS,
-        ///< found unescaped characters in a string.
-        UNCLOSED_STRING,
-        ///< missing quote at the end
-        UNSUPPORTED_ARCHITECTURE,
-        ///< unsupported architecture
-        INCORRECT_TYPE,
-        ///< JSON element has a different type than user expected
-        NUMBER_OUT_OF_RANGE,
-        ///< JSON number does not fit in 64 bits
-        INDEX_OUT_OF_BOUNDS,
-        ///< JSON array index too large
-        NO_SUCH_FIELD,
-        ///< JSON field not found in object
-        IO_ERROR,
-        ///< Error reading a file
-        INVALID_JSON_POINTER,
-        ///< Invalid JSON pointer reference
-        INVALID_URI_FRAGMENT,
-        ///< Invalid URI fragment
-        UNEXPECTED_ERROR,
-        ///< indicative of a bug in simdjson
-        /** @private Number of error codes */
-        NUM_ERROR_CODES,
+    
+    
+    struct ElementResult {
+        ptr: UniquePtr<element>,
+        error: i32,
     }
-
+   
+    
     extern "C" {
         include!("csrc/wrapper.h");
         type parser;
         type element;
-        type padded_string;
-        // type tape_ref;
+        // type padded_string;
+        // // type tape_ref;
 
-        type array;
-        type object;
+        // type array;
+        // type object;
 
-        type error_code;
+        // type error_code;
+        // type simdjson_result;
 
         fn parser_new(max_capacity: usize) -> UniquePtr<parser>;
-        fn parser_load(p: &mut parser, path: &str) -> Result<UniquePtr<element>>;
-        fn parser_parse_string(p: &mut parser, s: &str) -> Result<UniquePtr<element>>;
-        fn parser_parse_padded_string(
-            p: &mut parser,
-            s: &padded_string,
-        ) -> Result<UniquePtr<element>>;
+        fn parser_load(p: &mut parser, path: &str) -> ElementResult;
+        // fn parser_parse_string(p: &mut parser, s: &str, code: &mut i32) -> UniquePtr<element>;
+        // fn parser_parse_padded_string(
+        //     p: &mut parser,
+        //     s: &padded_string,
+        //     code: &mut i32,
+        // ) -> UniquePtr<element>;
 
-        fn padded_string_from_string(s: &str) -> UniquePtr<padded_string>;
+        // fn padded_string_from_string(s: &str) -> UniquePtr<padded_string>;
 
         // fn tape_ref_type(tr: &tape_ref) -> u8;
         // fn tape_ref_next_tape_value(tr: &tape_ref) -> u64;
 
-        fn element_get_string(elm: &element) -> Result<&str>;
-        fn element_get_array(elm: &element) -> Result<UniquePtr<array>>;
-        fn element_get_object(elm: &element) -> Result<UniquePtr<object>>;
-        fn element_get_number(elm: &element) -> Result<u64>;
-        fn element_is_null(elm: &element) -> bool;
-        fn element_at(elm: &element, json_pointer: &str) -> Result<UniquePtr<element>>;
-        fn element_at_index(elm: &element, index: usize) -> Result<UniquePtr<element>>;
-        fn element_at_key(elm: &element, key: &str) -> Result<UniquePtr<element>>;
+        // fn element_get_string(elm: &element, code: &mut i32) -> UniquePtr<CxxString>;
+        // fn element_get_array(elm: &element) -> Result<UniquePtr<array>>;
+        // fn element_get_object(elm: &element) -> Result<UniquePtr<object>>;
+        // fn element_get_number(elm: &element) -> Result<u64>;
+        // fn element_is_null(elm: &element) -> bool;
+        // fn element_at(elm: &element, json_pointer: &str) -> Result<UniquePtr<element>>;
+        // fn element_at_index(elm: &element, index: usize) -> Result<UniquePtr<element>>;
+        // fn element_at_key(elm: &element, key: &str) -> Result<UniquePtr<element>>;
 
-        fn array_at(arr: &array, json_pointer: &str) -> Result<UniquePtr<element>>;
-        fn array_at_index(arr: &array, index: usize) -> Result<UniquePtr<element>>;
+        // fn array_at(arr: &array, json_pointer: &str) -> Result<UniquePtr<element>>;
+        // fn array_at_index(arr: &array, index: usize) -> Result<UniquePtr<element>>;
 
-        fn object_at(obj: &object, json_pointer: &str) -> Result<UniquePtr<element>>;
-        fn object_at_key(obj: &object, key: &str) -> Result<UniquePtr<element>>;
-        fn object_at_key_case_insensitive(obj: &object, key: &str) -> Result<UniquePtr<element>>;
+        // fn object_at(obj: &object, json_pointer: &str) -> Result<UniquePtr<element>>;
+        // fn object_at_key(obj: &object, key: &str) -> Result<UniquePtr<element>>;
+        // fn object_at_key_case_insensitive(obj: &object, key: &str) -> Result<UniquePtr<element>>;
 
     }
+
+    
 }
 
 pub const SIMDJSON_MAXSIZE_BYTES: usize = 0xFFFFFFFF;
@@ -112,18 +68,24 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 
-    #[test]
-    fn get_parser() {
-        let mut parser = ffi::parser_new(SIMDJSON_MAXSIZE_BYTES);
-        let element = ffi::parser_load(&mut parser, "json-examples/twitter.json").unwrap();
-        // dbg!(parser.load);
-        let element = ffi::parser_parse_string(&mut parser, "[1]").unwrap();
-    }
+    // #[test]
+    // fn get_parser() {
+    //     let mut parser = ffi::parser_new(SIMDJSON_MAXSIZE_BYTES);
+    //     let mut code = 0;
+    //     let element = ffi::parser_load(&mut parser, "json-examples/twitter.json", &mut code);
+    //     // dbg!(parser.load);
+    //     println!("parse code: {}", code);
+    //     let element = ffi::parser_parse_string(&mut parser, "[1]", &mut code);
 
-    #[test]
-    fn parse_padded_string() {
-        let ps = ffi::padded_string_from_string("[1]");
-        let mut parser = ffi::parser_new(SIMDJSON_MAXSIZE_BYTES);
-        let element = ffi::parser_parse_padded_string(&mut parser, &ps).unwrap();
-    }
+    //     println!("parse code: {}", code);
+    // }
+
+    // #[test]
+    // fn parse_padded_string() {
+    //     let ps = ffi::padded_string_from_string("[1]");
+    //     let mut parser = ffi::parser_new(SIMDJSON_MAXSIZE_BYTES);
+    //     let mut code = 0;
+    //     let element = ffi::parser_parse_padded_string(&mut parser, &ps, &mut code);
+    //     println!("parse code: {}", code);
+    // }
 }

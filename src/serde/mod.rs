5460c90550c29a85b2fa6f4b43d40793f1bec831
SunDoge <384813529@qq.com>
2020-06-14 22:29:32 +0800

    Update ci on Windows. Update README

--- src/serde/mod.rs
+++ src/serde/mod.rs
@@ -8,22 +8,22 @@ mod tests {
     use serde::Deserialize;
 
     #[test]
-    fn test_element() {
+    fn test_element() -> Result<(), Box<dyn std::error::Error>> {
+        
+
+        let mut parser = Parser::default();
+        let elm = parser.parse_str(r#"[true, false]"#)?;
+        let a: Vec<bool> = de::from_element(&elm)?;
+        assert_eq!(vec![true, false], a);
+
         #[derive(Debug, Deserialize)]
         struct A {
             field1: bool,
         }
+        let elm = parser.parse_str(r#"{"field1": false}"#)?;
+        let a: A = de::from_element(&elm)?;
+        assert_eq!(a.field1, false);
 
-        let mut parser = Parser::default();
-        // let elm = parser.parse_str(r#"{"field1": true}"#).unwrap();
-
-        let elm = parser.parse_str(r#"[true, false]"#).unwrap();
-
-        let a: Vec<bool> = de::from_element(&elm).unwrap();
-        println!("{:?}", a);
-
-        let elm = parser.parse_str(r#"{"field1": false}"#).unwrap();
-        let a: A = de::from_element(&elm).unwrap();
-        println!("{:?}", a);
+        Ok(())
     }
 }

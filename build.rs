d8b6b528df6489d96c701e80643b9b80b888a86a
SunDoge <384813529@qq.com>
2020-06-14 23:16:45 +0800

    Update /std:c++latest

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() {
         .file("csrc/wrapper.cpp")
         .file("csrc/simdjson.cpp")
         .flag_if_supported("-std=c++17")
-        .flag_if_supported("/std:c++17") // For windows
+        .flag_if_supported("/std:c++latest") // For windows
         .compile("simdjson-sys");
 
     println!("cargo:rerun-if-changed=src/main.rs");

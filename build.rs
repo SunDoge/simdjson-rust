dcf315eec0d71770a321868a059bd6f16c1ee1b3
SunDoge <384813529@qq.com>
2022-12-07 02:18:50 +0000

    set SIMDJSON_DEVELOPER_MODE

--- build.rs
+++ build.rs
@@ -22,6 +22,13 @@ fn main() {
 
     let _build = cxx_build::bridge("src/bridge.rs");
 
+    let mut config = Config::new(".");
+    let profile = config.get_profile();
+
+    if profile == "Debug" {
+        config.define("SIMDJSON_DEVELOPER_MODE", "ON");
+    }
+
     let dst = Config::new(".").build().join("lib");
 
     println!("cargo:rerun-if-changed=src/bridge.rs");

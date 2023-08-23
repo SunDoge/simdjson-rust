b485e713324ac8d6c523c1b3b286048c6a02becf
SunDoge <384813529@qq.com>
2023-08-23 20:20:59 +0800

    macro macro macro

--- simdjson-sys/src/simdjson_c_api.cpp
+++ simdjson-sys/src/simdjson_c_api.cpp
@@ -1,23 +1,95 @@
 #include "simdjson_c_api.h"
 #include "simdjson.h"
 
-#define IMPL_FREE(name)                                                          \
-    void SJ_##name##_free(SJ_##name *r)                                          \
-    {                                                                            \
-        delete reinterpret_cast<simdjson::name *>(r);                            \
-    }                                                                            \
-    void SJ_simdjson_result_of_##name##_free(SJ_simdjson_result_of_##name *r)    \
-    {                                                                            \
-        delete reinterpret_cast<simdjson::simdjson_result<simdjson::name> *>(r); \
+using namespace simdjson;
+
+namespace
+{
+    template <typename U, typename T>
+    U *object_to_pointer(T &&t)
+    {
+        return reinterpret_cast<U *>(new T(std::move(t)));
     }
 
-IMPL_FREE(padded_string)
+}
+
+#define IMPL_CLASS(name, type)              \
+    void name##_free(name *r)               \
+    {                                       \
+        delete reinterpret_cast<type *>(r); \
+    }
+
+// Use value_unsafe because we always check the error code first.
+#define IMPL_RESULT(name, type)                                                  \
+    IMPL_CLASS(name##_result, simdjson_result<type>)                             \
+    int name##_result_error(const name##_result *r)                              \
+    {                                                                            \
+        auto code = reinterpret_cast<const simdjson_result<type> *>(r)->error(); \
+        return static_cast<int>(code);                                           \
+    }                                                                            \
+    name *name##_result_value(name##_result *r)                                  \
+    {                                                                            \
+        auto result = reinterpret_cast<simdjson_result<type> *>(r);              \
+        return object_to_pointer<name>(std::move(*result).value_unsafe());       \
+    }
+
+#define IMPL_PRIMITIVE_RESULT(name)                                              \
+    IMPL_CLASS(name##_result, simdjson_result<name>)                             \
+    int name##_result_error(const name##_result *r)                              \
+    {                                                                            \
+        auto code = reinterpret_cast<const simdjson_result<name> *>(r)->error(); \
+        return static_cast<int>(code);                                           \
+    }                                                                            \
+    name name##_result_value(name##_result *r)                                   \
+    {                                                                            \
+        auto result = reinterpret_cast<simdjson_result<name> *>(r);              \
+        return std::move(*result).value_unsafe();                                \
+    }
+
+IMPL_CLASS(SJ_padded_string, padded_string)
+IMPL_RESULT(SJ_padded_string, padded_string)
+IMPL_CLASS(SJ_OD_parser, ondemand::parser)
+IMPL_CLASS(SJ_OD_document, ondemand::document)
+IMPL_RESULT(SJ_OD_document, ondemand::document)
+
+IMPL_PRIMITIVE_RESULT(uint64_t)
+IMPL_PRIMITIVE_RESULT(int64_t)
+IMPL_PRIMITIVE_RESULT(double)
+IMPL_PRIMITIVE_RESULT(bool)
+IMPL_PRIMITIVE_RESULT(SJ_string_view);
 
 SJ_padded_string *SJ_padded_string_new(const char *s, size_t len)
 {
-    return reinterpret_cast<SJ_padded_string *>(new simdjson::padded_string(s, len));
+    // return reinterpret_cast<SJ_padded_string *>(new simdjson::padded_string(s, len));
+    return object_to_pointer<SJ_padded_string>(padded_string(s, len));
+}
+SJ_padded_string_result *SJ_padded_string_load(const char *path)
+{
+    // return reinterpret_cast<SJ_padded_string_result *>(new simdjson_result<padded_string>(padded_string::load(path)));
+    return object_to_pointer<SJ_padded_string_result>(padded_string::load(path));
 }
 
-// void SJ_padded_string_free(SJ_padded_string* r) {
-//     delete reinterpret_cast<simdjson::padded_string*>(r);
-// } 
+SJ_OD_parser *SJ_OD_parser_new(size_t max_capacity)
+{
+    // return reinterpret_cast<SJ_OD_parser *>(new ondemand::parser(max_capacity));
+    return object_to_pointer<SJ_OD_parser>(ondemand::parser(max_capacity));
+}
+
+SJ_OD_document_result *SJ_OD_parser_iterate_padded_string(SJ_OD_parser *parser, const SJ_padded_string *s)
+{
+    auto doc = reinterpret_cast<ondemand::parser *>(parser)->iterate(*reinterpret_cast<const padded_string *>(s));
+    // return reinterpret_cast<SJ_OD_document_result *>(new simdjson_result<ondemand::document>(std::move(doc)));
+    return object_to_pointer<SJ_OD_document_result>(std::move(doc));
+}
+
+SJ_OD_document_result *SJ_OD_parser_iterate_padded_string_view(SJ_OD_parser *parser, const char *json, size_t len, size_t allocated)
+{
+    auto doc = reinterpret_cast<ondemand::parser *>(parser)->iterate(padded_string_view(json, len, allocated));
+    return object_to_pointer<SJ_OD_document_result>(std::move(doc));
+}
+
+SJ_OD_value_result *SJ_OD_document_get_value(SJ_OD_document *doc)
+{
+    auto value = reinterpret_cast<ondemand::document *>(doc)->get_value();
+    return object_to_pointer<SJ_OD_value_result>(std::move(value));
+}
--- simdjson-sys/src/simdjson_c_api.h
+++ simdjson-sys/src/simdjson_c_api.h
@@ -1,29 +1,88 @@
 #pragma once
 
+#include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
-#define FUNC_FOR_RESULT(name)                                                                  \
-    typedef struct SJ_simdjson_result_of_##name SJ_simdjson_result_of_##name;                  \
-    int SJ_simdjson_result_of_##name##_error(const SJ_simdjson_result_of_##name *r); \
-    SJ_##name *SJ_simdjson_result_of_##name##_value(const SJ_simdjson_result_of_##name *r);
+#define DEFINE_CLASS(name)    \
+    typedef struct name name; \
+    void name##_free(name *r);
 
-#define FUNC_FOR_OBJECT(name) \
-    typedef struct SJ_##name SJ_##name; \
-    void SJ_##name##_free(SJ_##name *r);
+// `value` method will take the ownership of T.
+#define DEFINE_RESULT(name)                          \
+    DEFINE_CLASS(name##_result)                      \
+    int name##_result_error(const name##_result *r); \
+    name *name##_result_value(name##_result *r);
+
+// `value` method will free simdjson_result.
+#define DEFINE_PRIMITIVE_RESULT(name)                \
+    DEFINE_CLASS(name##_result)                      \
+    int name##_result_error(const name##_result *r); \
+    name name##_result_value(name##_result *r);
+
+#define DEFINE_GET(self, value) \
+    value##_result *self##_get_##value(self *r);
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-    // SJ = simdjson::*
-    FUNC_FOR_OBJECT(padded_string)
-    FUNC_FOR_RESULT(padded_string)
+    // SJ for simdjson, OD for ondemand
+    DEFINE_CLASS(SJ_padded_string);
+    DEFINE_RESULT(SJ_padded_string);
+    DEFINE_CLASS(SJ_OD_parser);
+    DEFINE_CLASS(SJ_OD_document);
+    DEFINE_RESULT(SJ_OD_document);
+    DEFINE_CLASS(SJ_OD_value);
+    DEFINE_RESULT(SJ_OD_value);
+    DEFINE_CLASS(SJ_OD_array);
+    DEFINE_RESULT(SJ_OD_array);
+    DEFINE_CLASS(SJ_OD_object);
+    DEFINE_RESULT(SJ_OD_object);
+    DEFINE_CLASS(SJ_OD_raw_json_string);
+    DEFINE_RESULT(SJ_OD_raw_json_string);
+
+    DEFINE_PRIMITIVE_RESULT(uint64_t);
+    DEFINE_PRIMITIVE_RESULT(int64_t);
+    DEFINE_PRIMITIVE_RESULT(double);
+    DEFINE_PRIMITIVE_RESULT(bool);
 
     SJ_padded_string *SJ_padded_string_new(const char *s, size_t len);
-    SJ_simdjson_result_of_padded_string SJ_padded_string_load(const char *path); // null terminated string.
+    SJ_padded_string_result *SJ_padded_string_load(const char *path); // null terminated string.
 
+    SJ_OD_parser *SJ_OD_parser_new(size_t max_capacity);
+    SJ_OD_document_result *SJ_OD_parser_iterate_padded_string(SJ_OD_parser *parser, const SJ_padded_string *s);
+    SJ_OD_document_result *SJ_OD_parser_iterate_padded_string_view(SJ_OD_parser *parser, const char *json, size_t len, size_t allocated);
 
+    SJ_OD_value_result *SJ_OD_document_get_value(SJ_OD_document *doc);
+    DEFINE_GET(SJ_OD_value, uint64_t);
+    DEFINE_GET(SJ_OD_value, int64_t);
+    DEFINE_GET(SJ_OD_value, double);
+    DEFINE_GET(SJ_OD_value, bool);
+    DEFINE_GET(SJ_OD_value, SJ_OD_array);
+    DEFINE_GET(SJ_OD_value, SJ_OD_object);
+    DEFINE_GET(SJ_OD_value, SJ_OD_raw_json_string);
+
+    DEFINE_GET(SJ_OD_document, uint64_t);
+    DEFINE_GET(SJ_OD_document, int64_t);
+    DEFINE_GET(SJ_OD_document, double);
+    DEFINE_GET(SJ_OD_document, bool);
+    DEFINE_GET(SJ_OD_document, SJ_OD_array);
+    DEFINE_GET(SJ_OD_document, SJ_OD_object);
+    DEFINE_GET(SJ_OD_document, SJ_OD_raw_json_string);
+
+    typedef struct SJ_string_view
+    {
+        const char *ptr;
+        size_t len;
+    } SJ_string_view;
+    DEFINE_PRIMITIVE_RESULT(SJ_string_view); // Does not holding the memory.
+    SJ_string_view_result *SJ_OD_value_get_string(SJ_OD_value *value, bool allow_replacement);
+    SJ_string_view_result *SJ_OD_document_get_string(SJ_OD_document *doc);
+
+    SJ_string_view_result *SJ_OD_value_get_wobbly_string(SJ_OD_value *value);
+    SJ_string_view_result *SJ_OD_document_get_wobbly_string(SJ_OD_document *doc);
 
 #ifdef __cplusplus
 }
